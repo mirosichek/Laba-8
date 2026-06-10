8a84adc2be0117e4a02d58d90d59fc3f5a8d76ad
mirosick <mirosickimya@gmail.com>
2026-06-10 12:38:14 +0300

    New file test_cards.py

---
+++ test_cards.py
@@ -0,0 +1,18 @@
+from cards import DebitCard, CreditCard
+from payment_system import Visa, MasterCard
+
+def test_debit_card_with_visa(capsys):
+    card = DebitCard(Visa())
+
+    card.pay()
+
+    captured = capsys.readouterr()
+    assert captured.out.strip() == "Дебетовая карта: Оплата через Visa"
+
+def test_test_credit_card_with_mastercard(capsys):
+    card=CreditCard(MasterCard())
+
+    card.pay()
+
+    captured = capsys.readouterr()
+    assert captured.out.strip() == "Кредитная карта: Оплата через MasterCard"
