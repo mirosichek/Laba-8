74b7db2744e262425b7ab01a072b40d94dc43516
mirosick <mirosickimya@gmail.com>
2026-06-10 12:19:02 +0300

    Only pay system classes

--- payment_system.py
+++ payment_system.py
@@ -1,29 +1,26 @@
 from abc import ABC, abstractmethod
 
+
 class PaymentSystem(ABC):
 
     @abstractmethod
     def process_payment(self):
         pass
 
+
 class Visa(PaymentSystem):
+
     def process_payment(self):
         return "Оплата через Visa"
-    
 
-class Card(ABC):
-    def _init_(self, payment_system):
-        self.payment_system=payment_system
 
-    @abstractmethod
-    def pay(self):
-        pass
+class MasterCard(PaymentSystem):
 
-class DebitCard(Card):
-    def pay(self):
-        print(f"Дебетовая карта: {self.payment_system.process_payment()}")
+    def process_payment(self):
+        return "Оплата через MasterCard"
 
-class CreditCard(Card):
 
-    def pay(self):
-        print(f"Кредитная карта: {self.payment_system.process_payment()}")
+class Mir(PaymentSystem):
+
+    def process_payment(self):
+        return "Оплата через Мир"
